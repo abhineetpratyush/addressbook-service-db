27d37becd848f005620716cd13c4e77a1eba066c
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-29 02:13:24 +0530

    [abhineet] add : abilitiy to create addressbook table with given fiels

--- AddressbookService.sql
+++ AddressbookService.sql
@@ -8,4 +8,17 @@ create database addressbook_service;
 
 use addressbook_service;
 
+# Create addressbook table
 
+create table addressbook
+(
+   first_name    varchar(150) not null,
+   last_name     varchar(150) not null,
+   address       varchar(400), 
+   city	   	 varchar(20),
+   state	 varchar(20),
+   zip		 int unsigned,
+   phone_number	 int unsigned,
+   email_id    	 varchar(100) not null,
+   primary key   (email_id)
+);
